5138a5a8e0aa18b530c45ca961d62962c8fa5e34
Aryann15 <rajlysm15@gmail.com>
2023-07-12 01:13:44 +0530

    reading and resizing all images

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+from skimage.io import imread
+from skimage.transform import resize
+
 
 input_dir = 'data'
 categories = ['empty','not_empty']
@@ -6,4 +9,8 @@ categories = ['empty','not_empty']
 data= []
 labels= []
 
-for
+for category in categories:
+    for file in os.listdir(os.path.join(input_dir,category)):
+        img_path = os.path.join(input_dir,category,file)
+        img= imread(img_path)
+        img = resize(img,(15,15))
